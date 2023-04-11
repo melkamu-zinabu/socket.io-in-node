3561568529d0074a256b41a6e17db4a37ed4223e
melkamu <zmelkamu918@gmail.com>
2023-04-12 00:25:45 +0300

    i am working on query string

--- clientt/src/component/chat/Chat.js
+++ clientt/src/component/chat/Chat.js
@@ -6,7 +6,7 @@ import io from 'socket.io-client'
 export const Chat = ({qs}) => {
   //run when thr function renders
   useEffect(()=>{
-    const {name,room}=queryString.parse(qs)
+    const {name,room}=queryString.parse()
     console.log(name,room)
   })
   return (
